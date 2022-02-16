3c64dc4bc3893dfdc7dd02c87dbd165151da6cb8
Luis García <luigui.hades@gmail.com>
2022-02-16 11:04:43 -0400

    Seccion  1: variables

---
+++ 01 lession 1/variables.py
@@ -0,0 +1,29 @@
+# la variable es facil de hacer solo debemos utilizar el simbolo de =
+miVariable = "hola mundo desde python clase1"
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+# podemos cambiar el valor de la variable usado el nombre de la misma
+miVariable = 10
+print(miVariable)
+
+
+# sumar, restar, dividir y multiplicar mas de 2 variables e introducirlas  a una tercera
+x = 10
+y = 11
+SUMA = x + y
+RESTA = x - y
+MULTIPLICACION = x * y
+DIVISION = x / y
+print(SUMA)
+print(RESTA)
+print(MULTIPLICACION)
+print(DIVISION)
+
+# DIRECCION DE MEMORIA SE ENCUENTRA  CON LA FUNCION id(NOMBRE DE LA VARIABLE)
+# Y PARA VERLA EN CONSOLA USAMOS LA FUNCION PRINT
+print(id(x))
+
+# HAY QUE RECORDAR QUE LOS DATOS EN MEMORIA RAM SACADOS CON LA FUNCION id() SON VOLATILES Y CAMIBIA A MEDIDA QUE SE
+# EJECUTA EL ARCHIVO
