066a85ec34c5f61116a3d2285ca0c7516a836cc4
Luis García <luigui.hades@gmail.com>
2022-02-17 12:52:41 -0400

    tipoos de boolean y procesar datos con input()

---
+++ 01 lession 1/04-TiposBooleanos.py
@@ -0,0 +1,19 @@
+# los tipos de datos bool (boolean) son True u False
+vVerdadera = True
+vFalsa = False
+print("Variable verdadera:", vVerdadera)
+print("Variable falsa:", vFalsa)
+
+
+# si hacemos una comparacion de mayor que o menor que nos mostrara la variable booleana que corresponta
+
+Comparación = 3 > 1
+print("Tres es mayor que uno:", Comparación)
+
+
+# se puede usar la comparacion para dar resultados en condicional if
+if Comparación:
+  	print("La comparación es verdadera")
+else:
+  	print("La comparación es falsa")
+
---
+++ 01 lession 1/05-ProcesarEntradaDeDatos.py
@@ -0,0 +1,28 @@
+# Función input para procesar la entrada del usuario
+resultado = input("Escribe un mensaje: ")
+print("Valor proporcionado:",resultado)
+print("Fin del programa")
+
+#parte 2
+# convercion de la entrada de datos en python
+#se usa la funcion int() para combertir en numerico los numeros ya que input como funcion arroja como resultado
+# un str
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe el segundo número: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es:", resultado)
+
+
+dia = int(input("Cómo estuvo tu día (1 a 10): "))
+print("Mi día estuvo de:", dia)
+
+
+
+
+
+
+
+
+
+
+
