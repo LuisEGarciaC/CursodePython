5dd3d9c1b9087bf1de4369f254fa88b9d7e047c1
Luis García <luigui.hades@gmail.com>
2022-02-17 16:52:24 -0400

    ejercicios guiados

---
+++ 02 lession operadores/EjerciciosGuiados/01-Par-Impar.py
@@ -0,0 +1,10 @@
+# EJERCICIO 1
+# evaluar si un numero ingresado es par o impar
+a = int(input('Ingrese el número a evaluar: '))
+
+if ((a % 2) == 0):
+  print(f"El número {a} es par")
+else:
+  print(f"El número {a} es impar")
+
+
---
+++ 02 lession operadores/EjerciciosGuiados/02-Mayoriadeedad.py
@@ -0,0 +1,10 @@
+#Ejercicio para
+# Determinar la mayoria de dedad
+mayoriaEdad = 18
+edad = int(input('Escribe tu edad: '))
+
+# usamos el condicional if
+if edad >= mayoriaEdad:
+	print(f'Eres mayor de edad ya que tienes {edad} años')
+else:
+	print(f'Eres menor de edad ya que tienes {edad} años')
