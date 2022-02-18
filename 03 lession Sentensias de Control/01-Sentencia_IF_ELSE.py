79eadd6fd8c993a1f3be4280f4e051dd3afb46e9
Luis García <luigui.hades@gmail.com>
2022-02-18 19:39:37 -0400

    Condicional IF /ELIF/ ELSE y ejercicios

---
+++ 03 lession Sentensias de Control/01-Sentencia_IF_ELSE.py
@@ -0,0 +1,10 @@
+# PRIMERA CONDICIONAL IF
+# cualquier valor q no sea vacio es verdadero para python
+condicion = "0"
+
+if condicion == True:
+    print('Condición verdadera')
+elif condicion == False:
+    print('Condición falsa')
+else:
+    print('Condición no reconocida')
