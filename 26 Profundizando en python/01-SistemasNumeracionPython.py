2cb2206c389ca6dbeb2b9589c5521aed867db549
Luis <luigui.hades@gmail.com>
2022-07-12 21:09:15 -0400

    Profundizando en python Números

---
+++ 26 Profundizando en python/01-SistemasNumeracionPython.py
@@ -0,0 +1,18 @@
+# Profundizando Sistemas Numeración
+
+"""
+	en python se puede usa los valores numericos de distintas formas
+	para binario usamos 0b
+	para octal 0o
+	para hexadecimal es 0x
+"""
+
+# Decimal
+a = 10
+# Binario
+a = 0b1010
+# Octal
+a = 0o12
+# Hexadecimal
+a = 0xA
+print(f'a: {a}')
